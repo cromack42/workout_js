b4514a21c384b576d59cdc32e6d082d8e683d0f3
Marcelo Cromack <74430450+cromack42@users.noreply.github.com>
2022-08-16 09:09:58 -0300

    Add files via upload

---
+++ arrays/push.js
@@ -0,0 +1,13 @@
+/* O push() é um método onde adiciona se um ou mais elementos no fim de uma array 
+e retorna o novo comprimento da mesma */
+
+let animais = ['porco', 'cabra', 'ovelha']
+
+const conta = animais.push('vaca')
+console.log('Matriz (Array) adicionada: ', conta, animais)
+
+animais.push('galinha', 'gato', 'cachorro')
+console.log(animais)
+
+/* O método push() adiciona valor para uma array. */
+
---
+++ arrays/unshift.js
@@ -0,0 +1,8 @@
+/* O método unshift() adiciona um ou mais elementos 
+no começo de uma array e retorna um novo comprimento 
+para a mesma. */
+
+let array1 = [1,2,3]
+
+console.log(array1.unshift(4,5))
+console.log(array1)
